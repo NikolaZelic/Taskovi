8134f3db78a45311d8c636f0b96763e5ee701256
Milos Paunovic <paun992@hotmail.com>
2018-05-24 15:16:18 +0200

    Dodate tri komponente radne povrsine (sredjen samo vizuelni prikaz - funkcionalnost i dalje ne)

--- src/components/Merged.vue
+++ src/components/Merged.vue
@@ -1,7 +1,11 @@
 <template lang="html">
   <div id="wrapper">
    <left-sidebar/>
-   <div class="main-content"></div>
+   <div class="main-content">
+     <!-- <task-view></task-view> -->
+     <task-add></task-add>
+     <!-- <project-add></project-add> -->
+   </div>
    <right-side/>
   </div>
 </template>
@@ -9,10 +13,17 @@
 <script>
 import LeftSidebar from "@/components/LeftSidebar"
 import RightSide from "@/components/RightSide"
+import TaskView from "@/components/TaskView"
+import TaskAdd from "@/components/TaskAdd"
+import ProjectAdd from "@/components/ProjectAdd"
+
 export default {
   components: {
     LeftSidebar,
     RightSide,
+    TaskView,
+    TaskAdd,
+    ProjectAdd
   },
 }
 </script>
---
+++ src/components/ProjectAdd.vue
@@ -0,0 +1,54 @@
+<template>
+<div>
+  <div class="row task-add-section">
+    <div class="col-md-8 offset-md-2 pad">
+
+      <h1 class="display-4">Adding project:</h1><br>
+
+      <form>
+        <div class="form-group">
+          <label for="tsk_title">Project title</label>
+          <input type="text" class="form-control" id="pro_title" placeholder="Enter the name of the project you're creating">
+        </div>
+
+        <label for="pro_leader">Project leader</label>
+        <div class="form-group input-group">
+          <input type="text" class="form-control" id="pro_leader" placeholder="Search for user who will be this project leader">
+          <div class="input-group-append">
+            <button class="btn btn-outline-secondary" type="button">Search</button>
+          </div>
+        </div>
+
+        <div class="form-group">
+          <button type="submit" class="btn btn-success">Add project</button>
+        </div>
+
+
+
+      </form>
+
+
+
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+// import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      working: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.task-add-section {
+  padding-top: 50px;
+}
+</style>
