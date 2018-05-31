4b5aad67fc186e510c61597295413d0ec5deea13
Milos Paunovic <paun992@hotmail.com>
2018-05-31 13:34:38 +0200

    a

--- src/components/MergedComp.vue
+++ src/components/MergedComp.vue
@@ -29,13 +29,21 @@
 import SideBar from "@/components/SideBar";
 
 import ChatElement from "@/components/Chat/ChatElement";
+
 import TaskView from "@/components/Content/TaskView";
 import TaskAdd from "@/components/Content/TaskAdd";
+import TeamAdd from "@/components/Content/TeamAdd";
+import TeamEdit from "@/components/Content/TeamEdit";
+
+
 import ProjectAdd from "@/components/Content/ProjectAdd";
 import ProjectView from "@/components/Content/ProjectView";
-import TeamAdd from "@/components/Content/TeamAdd";
+import ProjectEdit from "@/components/Content/ProjectEdit";
+
 import CompanyAdd from "@/components/Content/CompanyAdd";
 import CompanyEdit from "@/components/Content/CompanyEdit";
+import CompanyView from "@/components/Content/CompanyView";
+
 import Registration from "@/components/Auth/Registration";
 import { api } from "@/api/index.js";
 
