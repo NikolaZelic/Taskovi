30b44a9e6303c5b071abd033b21ceb3c3ab20d8b
Svetozar <svetozardavidovic94@gmail.com>
2018-06-15 17:03:02 +0200

    edit teams krenulo

--- src/components/Content/Team/TeamEdit.vue
+++ src/components/Content/Team/TeamEdit.vue
@@ -11,16 +11,9 @@
   </div>
 
   <!-- Adding new team members -->
-  <h4>Add new user:</h4>
-  <div class="input-group mb-5">
-    <input type="text" class="form-control" placeholder="Enter user's email address" v-model="useremail">
-    <div class="input-group-append">
-      <button class="btn btn-outline-secondary" type="button" @click="addAdmin()">As admin</button>
-      <button class="btn btn-outline-secondary" type="button" @click="addMember()">As employee</button>
-    </div>
-  </div>
-  <div class="mb-5" v-if="notExistingAdmin">{{message}}</div>
-  <div class="mb-5" v-if="notExistingMember">{{message}}</div>
+  <vue-autosuggest ref="suggestionTag" :suggestions="[ { data: suggestions } ]" :renderSuggestion="renderSuggestion" @click="clickHandler" :onSelected="onSelected" :inputProps="{class:'autosuggest__input', onInputChange: this.onInputChange, placeholder:'Enter user'}"
+    :getSuggestionValue="getSuggestionValue" />
+  </vue-autosuggest>
 
   <!-- List of team's admins -->
   <h4>Team's admins:</h4>
@@ -33,7 +26,7 @@
   <!-- List of team's employees -->
   <h4>Team's employees:</h4>
   <ul class="list-group list-group-flush mb-5">
-    <li class="list-group-item" v-for="employee in employees">
+    <li class="list-group-item" v-for="employee in members">
       {{employee.name}}<span class="small"> --- {{employee.email}}</span>
     </li>
   </ul>
@@ -42,14 +35,21 @@
 </template>
 
 <script>
-import axios from "axios";
-
+import {VueAutosuggest} from 'vue-autosuggest';
+import {store} from "@/store/index.js";
+import {api} from '@/api/index.js';
 export default {
+  components:{
+    VueAutosuggest
+  },
   data() {
     return {
+      teamId: store.state.itemAction.edit,
       teamname: "",
       useremail: "",
       message: "",
+
+      interval:null,
 
       notExistingAdmin: false,
       notExistingMember: false,
@@ -58,83 +58,58 @@ export default {
       members: []
     };
   },
-
+  computed:{
+    suggestions() {
+      return store.getters.getSuggestedUsers;
+    }
+  },
   methods: {
+
     changeTeamName() {
-      axios.put("LINKTOAPI", {
-        teamid: 13, //treba ubaciti iz stora? neki teamID onog tima koja je odabran u side meniju
-        teamname: this.teamname,
-        sid: window.localStorage.getItem("sid")
-      });
+
     },
 
     addAdmin() {
-      axios
-        .post("LINKTOAPI", {
-          teamid: 13, //treba ubaciti iz stora? neki teamID onog tima koja je odabran u side meniju
-          email: this.email,
-          sid: window.localStorage.getItem("sid")
-        })
-        .then(response => {
-          if (response.data.status === "ERR") {
-            this.notExistingAdmin = true;
-            this.message = response.data.message;
-          } else {
-            this.notExistingAdmin = false;
-          }
-          this.loadAdmins();
-        });
+
     },
 
     addMember() {
-      axios
-        .post("LINKTOAPI", {
-          teamid: 13, //treba ubaciti iz stora? neki teamID onog tima koja je odabran u side meniju
-          email: this.email,
-          sid: window.localStorage.getItem("sid")
-        })
-        .then(response => {
-          if (response.data.status === "ERR") {
-            this.notExistingMember = true;
-            this.message = response.data.message;
-          } else {
-            this.notExistingMember = false;
-          }
-          this.loadMembers();
-        });
-    },
 
+    },
+    loadTeam(){
+      api.getTeam(this.teamId).then(response=>{
+        this.teamname = response.data.data[0].title;
+      })
+    },
     loadAdmins() {
-      axios
-        .get("LINKTOAPI", {
-          params: {
-            teamid: 13, //treba ubaciti iz stora? neki teamID onog tima koja je odabran u side meniju
-            sid: window.localStorage.getItem("sid")
-          }
-        })
-        .then(response => {
-          this.admins = response.data.data;
-        });
+
+      api.getTeamAdmins(this.teamId).then(response=>{
+        console.log(response);
+        this.admins= response.data.data;
+      })
     },
 
     loadMembers() {
-      axios
-        .get("LINKTOAPI", {
-          params: {
-            teamid: 13, //treba ubaciti iz stora? neki teamID onog tima koja je odabran u side meniju
-            sid: window.localStorage.getItem("sid")
-          }
-        })
-        .then(response => {
-          this.members = response.data.data;
-        });
+      api.getTeamMembers(this.teamId).then(response=>{
+        this.members=response.data.data;
+      });
     }
   },
 
   mounted() {
-    this.loadAdmins();
+    this.loadTeam();
     this.loadMembers();
-  }
+    this.loadAdmins();
+    // this.interval = setInterval(() => {
+    //   if (this.haveChange === 1 && this.inputText.length > 0 && this.choosenCompany.id !== undefined) {
+    //     this.pozivapija();
+    //     this.haveChange = 0;
+    //   }
+    // }, 500);
+  },
+  destroy: function() {
+    clearInterval(interval);
+  },
 };
 </script>
 
