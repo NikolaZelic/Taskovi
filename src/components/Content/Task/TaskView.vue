72a5b08c7b270af7403957dba207298cc896c53a
Milos Paunovic <paun992@hotmail.com>
2018-06-29 16:44:57 +0200

    modalni prikaz pojedinacnog stepa

--- src/components/Content/Task/TaskView.vue
+++ src/components/Content/Task/TaskView.vue
@@ -7,8 +7,32 @@
   <template v-else>
 
 
-
-
+<!--
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+      Launch demo modal
+    </button>
+-->
+<!-- Modal
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+-->
 
 
 
@@ -32,7 +56,8 @@
             </thead>
             <tbody>
 
-              <tr v-for="(task, index) in taskInfo" :key='index' @click='getStepInfo(task.tsk_id)'> <!--@click="getStepInfo(task.tsk_id)"-->
+
+              <tr v-for="(task, index) in taskInfo" :key='index' @click='getStepInfo(task.tsk_id)' data-toggle="modal" data-target="#stepInformation">
                 <td>
                   <i class="fas fa-check-circle text-success" v-if="task.sta_text === 'Completed'"></i>
                   <i class="fas fa-spinner text-info" v-if="task.sta_text === 'In Progress' || task.sta_text === 'Assigned'"></i>
@@ -68,7 +93,11 @@
                 <td>
                 {{task.usrworking}}
               </td>
+
+
               </tr>
+
+
 
 
             </tbody>
@@ -80,9 +109,89 @@
 
 
 
+      <div class="modal fade" id="stepInformation" tabindex="-1" role="dialog" v-if="stepInfo.length > 0">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-header step-header" :class="stepInfo[0].background">
+              <h5 class="modal-title" id="exampleModalLabel">{{ stepInfo[0].tsk_title }}</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true" class="step-header">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <p><strong>Project: </strong>{{stepInfo[0].pro_name}}</p>
+              <p><strong>Task: </strong>{{stepInfo[0].taskname}}</p>
 
-      <div class="card mt-5 col-md-6 offset-md-3 pad-0 mb-5" v-if="stepInfo.length > 0">
-        <div class="card-header task-header">
+              <p><strong>Status: </strong>
+                {{stepInfo[0].sta_text}}
+                <i class="fas fa-check-circle text-success" v-if="stepInfo[0].sta_text === 'Completed'"></i>
+                <i class="fas fa-spinner text-info" v-if="stepInfo[0].sta_text === 'In Progress' || stepInfo[0].sta_text === 'Assigned'"></i>
+                <i class="fas fa-exclamation-triangle text-danger" v-if="stepInfo[0].sta_text === 'Failed' || stepInfo[0].sta_text === 'Rejected' || stepInfo[0].sta_text === 'Cancelled'"></i>
+              </p>
+
+              <p><strong>Description: </strong>{{stepInfo[0].description}}</p>
+              <p><strong>Priority: </strong><span class="badge" :class="stepInfo[0].pri_badge">{{stepInfo[0].pri_text}}</span></p>
+
+
+
+              <div>
+                <p><strong>Tags: </strong>
+                <span class="badge badge-success" v-for="tag in stepInfo[0].tags">{{ tag.tag_text }}</span>
+              </p>
+              </div>
+
+
+              <p><strong>Deadline: </strong>{{stepInfo[0].tsk_deadline}}</p>
+              <p><strong>Estimated completion date: </strong>{{stepInfo[0].tsk_estimated_completion_date}}</p>
+
+    <p><strong>Created by: </strong>
+      <ul class="list-unstyled">
+        <li class="media mt-2">
+
+          <img v-if='stepInfo[0].usr_picture === null' class="rounded-circle mr-3" height="50px" width="50px" src="@/assets/img/avatar.png" />
+          <img v-else class="rounded-circle mr-3" height="50px" width="50px" :src="'data:image/jpeg;base64,' + stepInfo[0].usr_picture" />
+
+          <div class="media-body">
+            <div class="media-body">
+                  <h5 class="mt-0 mb-1">{{stepInfo[0].usr_creator_name}} {{stepInfo[0].usr_creator_surname}}</h5>
+                    {{stepInfo[0].usr_email}}
+              </div>
+              </div>
+
+        </li>
+      </ul>
+    </p>
+              <p><strong>Time created: </strong>{{stepInfo[0].tsk_timecreated}}</p>
+              <p><strong>Time spent: </strong>{{stepInfo[0].tsk_timespent}}</p>
+              <p><strong>Working: </strong>
+                <ul class="list-unstyled">
+                  <li class="media mt-2" v-for="(user,index) in stepInfo[0].usrworking" :key='index'>
+
+                    <img v-if='user.usr_picture === null' class="rounded-circle mr-3" height="50px" width="50px" src="@/assets/img/avatar.png" />
+                    <img v-else class="rounded-circle mr-3" height="50px" width="50px" :src="'data:image/jpeg;base64,' + user.usr_picture" />
+
+                    <div class="media-body">
+                      <div class="media-body">
+                            <h5 class="mt-0 mb-1">{{user.usr_name}}</h5>
+                              {{user.usr_email}}
+                        </div>
+                        </div>
+
+                  </li>
+                </ul>
+              </p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+<!--
+      <div class="card mt-5 col-md-6 offset-md-3 pad-0 mb-5" v-if="stepInfo.length > 0" id="step">
+        <div class="card-header step-header " :class="stepInfo[0].background">
           {{ stepInfo[0].tsk_title }}
         </div>
         <div class="card-body">
@@ -97,7 +206,7 @@
           </p>
 
           <p><strong>Description: </strong>{{stepInfo[0].description}}</p>
-          <p><strong>Priority: </strong><span class="badge" :class="stepInfo[0].pri_badge">{{stepInfo[0].pri_text}}</span></p> <!--dodati badge alert -->
+          <p><strong>Priority: </strong><span class="badge" :class="stepInfo[0].pri_badge">{{stepInfo[0].pri_text}}</span></p>
           <p><strong>Deadline: </strong>{{stepInfo[0].tsk_deadline}}</p>
           <p><strong>Estimated completion date: </strong>{{stepInfo[0].tsk_estimated_completion_date}}</p>
 
@@ -141,7 +250,7 @@
 
         </div>
       </div>
-
+-->
 
       <!-- description:null
       pri_text:"Low"
@@ -164,7 +273,10 @@
 
     </template>
 </div>
+
+
 </template>
+
 
 <script>
 import {
@@ -192,6 +304,8 @@ export default {
   },
 
   methods: {
+
+
     getStepInfo(stepID) {
       // console.log('taskID' + taskID + ', stepID' + stepID);
       axios.get("http://682b121.mars1.mars-hosting.com/mngapi/tasks/:tasid/steps/:stepid", {
@@ -212,14 +326,28 @@ export default {
               if (this.stepInfo[i].tsk_timespent !== null) {
                 if (this.stepInfo[i].tsk_timespent <= 59) {
                   this.stepInfo[i].tsk_timespent = this.stepInfo[i].tsk_timespent + ' minutes'
-                }
-                else{
+                } else {
                   let minutes = this.stepInfo[i].tsk_timespent % 60;
                   let hours = parseInt(this.stepInfo[i].tsk_timespent / 60);
 
                   this.stepInfo[i].tsk_timespent = hours + ' hour(s), ' + minutes + ' minute(s)'
                 }
               }
+
+if (this.stepInfo[i].sta_text !== null) {
+
+  if (this.stepInfo[i].sta_text === 'Assigned' || this.stepInfo[i].sta_text === 'In Progress') {
+    this.stepInfo[i].background = 'bg-info'
+  } else if(this.stepInfo[i].sta_text === 'Failed' || this.stepInfo[i].sta_text === 'Rejected' || this.stepInfo[i].sta_text === 'Cancelled' ){
+      this.stepInfo[i].background = 'bg-danger'
+    }
+    else{
+      this.stepInfo[i].background = 'bg-success'
+    }
+}
+
+
+
 
 
               if (this.stepInfo[i].tsk_deadline === null) {
@@ -250,7 +378,18 @@ export default {
             }
 
           }
-        });
+
+
+          // this.stepInfo.tags = this.tas
+        })
+
+
+        // .then(response => {
+        //   window.location.href = "#step";
+        // });
+
+
+
     },
 
     getTaskInfo(taskID) {
@@ -294,6 +433,7 @@ export default {
   },
 
   computed: {
+
 
     selectedItemID() {
       var a = store.getters.selectedItemID;
@@ -356,6 +496,10 @@ h1 {
   color: #333 !important;
 }
 
+.step-header {
+  color: #fff !important;
+}
+
 .badge {
   margin: 0.125rem;
 }
@@ -371,4 +515,6 @@ h1 {
 .slika {
   border: 1px solid #333;
 }
+
 </style>
+h
