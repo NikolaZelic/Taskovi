7f38f134b7b58bb6d61c7ec676c6531589ec2096
Milos Paunovic <paun992@hotmail.com>
2018-07-13 12:49:14 +0200

    resavanje bagova

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 let instance = axios.create({
-  baseURL: 'http://695u121.mars-t.mars-hosting.com/mngapi/'  
+  baseURL: 'http://695u121.mars-t.mars-hosting.com/mngapi/'
 });
 export {instance}
