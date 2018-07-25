f147021b2f5873706bff5b44b1b0ae25dea820f2
Milos Paunovic <paun992@hotmail.com>
2018-07-25 17:49:38 +0200

    a

--- src/components/Content/Project/ProjectManage.vue
+++ src/components/Content/Project/ProjectManage.vue
@@ -262,6 +262,9 @@ export default {
           sid: localStorage.sid
         })
         .then(r => {
+          // napraviti preko mutacija
+          store.commit("itemActionReset");
+
           if (r.data.status === "OK") {
             store.commit("modalStatus", {
               ok: true,
