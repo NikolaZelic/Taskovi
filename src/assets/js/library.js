0e6e51390ae90ef14395905185e98dfbae4d3f97
Zex <nzelic@ymail.com>
2018-08-08 13:51:57 +0200

    fix lib

--- src/assets/js/library.js
+++ src/assets/js/library.js
@@ -1,26 +1,20 @@
 export const library = {
-    messagesHaveFeed(messages, feed){
-        // console.log(feed);
-        if(feed==undefined||feed.fed_id==undefined)
-            return false;
-        if(messages.length==0)
-            return false;
-        for(let i=0; i<messages.length; i++){
-            if(messages[i].fed_id==undefined){
-                // console.log('Doesn"t have it it');
-                return false;
-            }
-            if( messages[i].fed_id == feed.fed_id ){
-                // console.log('Have it');
-                return true;
-            }
-               
-        }
+  messagesHaveFeed(messages, feed) {
+    // console.log(feed);
+    if (feed == undefined || feed.fed_id == undefined)
+      return false;
+    if (messages.length == 0)
+      return false;
+    for (let i = 0; i < messages.length; i++) {
+      if (messages[i].fed_id == undefined) {
+        // console.log('Doesn"t have it it');
         return false;
       }
       if (messages[i].fed_id == feed.fed_id) {
+        // console.log('Have it');
         return true;
       }
+
     }
     return false;
   }
